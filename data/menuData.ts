35a80a8d2125922ffa37a62cf9c2d2e5e7c3b9a3
wptasmina <wptasmina@gmail.com>
2025-06-09 17:54:45 +0600

    Fix: added missing components or removed broken imports

--- data/menuData.ts
+++
@@ -1,9 +0,0 @@
-
- 
-//  type menuItems: Navbar[] =[
-//     { href: "/", label: "Home" },
-//     { href: "/about", label: "About" },
-//     { href: "/projects", label: "Projects" },
-//     { href: "/blogs", label: "Blogs" },
-//     { href: "/contact", label: "Contact" },
-//   ];
