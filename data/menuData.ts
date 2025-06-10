7cbb4c770466ea6f082fffb5f183d9edf444c2bc
wptasmina <wptasmina@gmail.com>
2025-06-10 14:43:46 +0600

    add menu data

---
+++ data/menuData.ts
@@ -0,0 +1,28 @@
+
+const menuData = [
+    {
+        title: 'Home',
+        href: '/',
+        icon: 'home',
+    },
+    {
+        title: 'About',
+        href: '/about',
+        icon: 'info',
+    },
+    {
+        title: 'Services',
+        href: '/services',
+        icon: 'cog',
+    },
+     {
+        title: 'Blog',
+        href: '/blog'
+    },
+    {
+        title: 'Contact',
+        href: '/contact',
+        icon: 'envelope',
+    }
+   
+]
