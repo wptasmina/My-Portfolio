3bba209f08f3fe1053944b3c74f1272f898cb2a5
wptasmina <wptasmina@gmail.com>
2025-06-12 20:56:42 +0600

    server site navbar add

--- components/shared/Header/Logo.tsx
+++ components/shared/Header/Logo.tsx
@@ -1,12 +1,20 @@
+// components/Navbar/ServerNavbar.tsx
 import Link from "next/link";
 
-export default function Logo() {
+export const Logo = () => {
   return (
-   <div className="flex items-center gap-2 group md:w-1/3 ">
-     <Link href={"/"}>
-      Logo
-    </Link>
-    <span className="text-primary-text font-geist-mono">ts</span>
-   </div>
-  )
-}
+    <div className="flex items-center gap-2">
+      <div className="relative overflow-hidden h-8 w-8 rounded-full">
+        <div className="absolute inset-0 bg-gradient-to-r from-heroText to-tertiary animate-spin-slow rounded-full [mask-image:linear-gradient(transparent,white)]" />
+        <div className="absolute inset-[2px] bg-content-text rounded-full flex items-center justify-center z-10">
+          <span className="font-bold font-desc-josefin bg-gradient-to-r from-heroText to-tertiary bg-clip-text text-transparent">
+            <Link href="/">TS</Link>
+          </span>
+        </div>
+      </div>
+      <h2 className="font-semibold font-geist-sans dark:text-white/90 text-content">
+        Tasmina Akter
+      </h2>
+    </div>
+  );
+};
