10fe9af0e68c058a3e13b398364d080900e0c644
wptasmina <wptasmina@gmail.com>
2025-06-13 17:58:19 +0600

    delete Banner section

--- components/HomeSection/HeroSection.tsx
+++ components/HomeSection/HeroSection.tsx
@@ -1,10 +1,10 @@
+import React from 'react'
 
-import { SpotlightPreview } from './SpotlightPreview'
-
-export default function HeroSection() {
+export const HeroSection = () => {
   return (
     <div>
-      <SpotlightPreview />
+      header
     </div>
   )
 }
+
