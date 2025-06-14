f7b8c20fc72ea3644bea33f128e0b6cc0d7d328d
wptasmina <wptasmina@gmail.com>
2025-06-14 12:34:06 +0600

    magic card add

---
+++ components/magic-card-demo.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { MagicCard } from "@/components/magicui/magic-card";
+import { useTheme } from "next-themes";
+
+export default function MagicCardDemo() {
+  const { theme } = useTheme();
+  return (
+    <Card className="p-0 max-w-sm w-full border-none wrapper shadow">
+      <MagicCard
+        gradientColor={theme === "dark" ? "#262626" : "#D9D9D955"}
+        className="p-0"
+      >
+        <CardHeader className="border-b border-border p-4 [.border-b]:pb-4">
+          <CardTitle>Login</CardTitle>
+          <CardDescription>
+            Enter your credentials to access your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="p-4">
+          <form>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="name@example.com" />
+              </div>
+              {/* <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" />
+              </div> */}
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="p-4 border-t border-border [.border-t]:pt-4">
+          <Button className="w-full">Sign In</Button>
+        </CardFooter>
+      </MagicCard>
+    </Card>
+  );
+}
