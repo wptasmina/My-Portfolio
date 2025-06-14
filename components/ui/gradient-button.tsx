0c2fb30c0c2dd0c3bef533eadbb49c005d1d25d3
wptasmina <wptasmina@gmail.com>
2025-06-14 11:48:03 +0600

    hero section button cursor pointer

--- components/ui/gradient-button.tsx
+++ components/ui/gradient-button.tsx
@@ -13,7 +13,7 @@ export function GradientButton({ icon, children, className, ...props }: Gradient
     <button
       {...props}
       className={cn(
-        "flex items-center gap-2 text-xs md:text-base md:px-5 md:py-2 px-4 py-2.5 rounded-full bg-gradient-to-b from-zinc-950 to-[#03a5f0] text-white focus:ring-2 focus:ring-heroText hover:shadow-xl transition duration-200",
+        "flex items-center gap-2 text-xs md:text-base md:px-5 md:py-2 px-4 py-2.5 rounded-full bg-gradient-to-b from-zinc-950 to-[#03a5f0] text-white focus:ring-2 focus:ring-heroText hover:shadow-xl transition duration-200 cursor-pointer",
         className
       )}
     >
