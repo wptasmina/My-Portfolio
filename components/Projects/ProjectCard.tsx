3c627f090911a696d1f3d8c9c1eb9b605cde0fc6
wptasmina <wptasmina@gmail.com>
2025-06-15 01:52:25 +0600

    Project cord add

--- components/Projects/ProjectCard.tsx
+++ components/Projects/ProjectCard.tsx
@@ -1,17 +1,11 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { BorderBeam } from "@/components/magicui/border-beam";
+
+import Link from "next/link";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card";
 import Image from "next/image";
 import { Project } from "@/types";
+import { Button } from "../ui/button";
 import { FaGithub, FaGlobe } from "react-icons/fa";
-import Link from "next/link";
+import { BorderBeam } from "../magicui/border-beam";
 
 export function ProjectCard({
   title,
@@ -43,9 +37,9 @@ export function ProjectCard({
       </CardContent>
       <CardFooter className="flex flex-wrap gap-2 mt-auto">
         <div className="flex items-center gap-4">
-          <Link href={githubLink} target="_blank" rel="noopener noreferrer">
+          <Link href={githubLink} target="_blank">
             <button
-              className="btnSocial shadow-[0.05rem_0.05rem_0.1rem_#ccc,-0.05rem_-0.05rem_0.1rem_#fff] 
+              className="w-10 h-10 flex items-center justify-center cursor-pointer rounded-full bg-black text-[#e6e6e6] font-semibold border transition duration-300 shadow-[0.05rem_0.05rem_0.1rem_#ccc,-0.05rem_-0.05rem_0.1rem_#fff] 
               border-[rgba(0,0,0,0.2)]  
              hover:shadow-[inset_0.05rem_0.05rem_0.1rem_#ccc,inset_-0.05rem_-0.05rem_0.1rem_#fff] "
             >
@@ -55,7 +49,7 @@ export function ProjectCard({
 
           <Link href={demoLink} target="_blank" rel="noopener noreferrer">
             <button
-              className="btnSocial shadow-[0.05rem_0.05rem_0.1rem_#ccc,-0.05rem_-0.05rem_0.1rem_#fff] 
+              className="w-10 h-10 flex items-center justify-center cursor-pointer rounded-full bg-black text-[#e6e6e6] font-semibold border transition duration-300 shadow-[0.05rem_0.05rem_0.1rem_#ccc,-0.05rem_-0.05rem_0.1rem_#fff] 
               border-[rgba(0,0,0,0.2)]  
              hover:shadow-[inset_0.05rem_0.05rem_0.1rem_#ccc,inset_-0.05rem_-0.05rem_0.1rem_#fff] "
             >
