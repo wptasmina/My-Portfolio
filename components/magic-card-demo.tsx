ff21025ad841919129b35d33e708b5844254d9fc
wptasmina <wptasmina@gmail.com>
2025-06-15 02:08:55 +0600

    MagicCardDemo

--- components/magic-card-demo.tsx
+++ components/magic-card-demo.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { MagicCard } from "@/components/magicui/magic-card";
+
 import { useTheme } from "next-themes";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import { MagicCard } from "./magicui/magic-card";
+
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Label } from "./ui/label";
 
 export default function MagicCardDemo() {
   const { theme } = useTheme();
