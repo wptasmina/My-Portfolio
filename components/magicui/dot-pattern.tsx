9800aa52df87c827d366e7e0a434e1fbbe8b56f2
wptasmina <wptasmina@gmail.com>
2025-06-15 13:53:51 +0600

    add dot pattern

--- components/magicui/dot-pattern.tsx
+++ components/magicui/dot-pattern.tsx
@@ -4,12 +4,9 @@ import { cn } from "@/lib/utils";
 import { motion } from "motion/react";
 import React, { useEffect, useId, useRef, useState } from "react";
 
-
 interface DotPatternProps extends React.SVGProps<SVGSVGElement> {
   width?: number;
   height?: number;
-  x?: number;
-  y?: number;
   cx?: number;
   cy?: number;
   cr?: number;
@@ -18,12 +15,9 @@ interface DotPatternProps extends React.SVGProps<SVGSVGElement> {
   [key: string]: unknown;
 }
 
-
 export function DotPattern({
   width = 16,
   height = 16,
-  x = 0,
-  y = 0,
   cx = 1,
   cy = 1,
   cr = 1,
@@ -82,7 +76,7 @@ export function DotPattern({
           <stop offset="100%" stopColor="currentColor" stopOpacity="0" />
         </radialGradient>
       </defs>
-      {dots.map((dot, index) => (
+      {dots.map((dot) => (
         <motion.circle
           key={`${dot.x}-${dot.y}`}
           cx={dot.x}
