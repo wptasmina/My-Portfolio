84d06754a6a070959608c24b241cf88160df7b92
wptasmina <wptasmina@gmail.com>
2025-06-15 18:47:12 +0600

    sparkles add

--- components/ui/sparkles.tsx
+++ components/ui/sparkles.tsx
@@ -1,11 +1,11 @@
 "use client";
-import React, { useId, useMemo } from "react";
+import React, { useId } from "react";
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import type { Container, SingleOrMultiple } from "@tsparticles/engine";
 import { loadSlim } from "@tsparticles/slim";
 import { cn } from "@/lib/utils";
-import { motion, useAnimation } from "motion/react";
+import { motion, useAnimation } from "framer-motion";
 
 type ParticlesProps = {
   id?: string;
